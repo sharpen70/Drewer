4bb957d9f6d54a1c7700e5aeccb91587d170e569
sharpen70 <sharpen70@gmail.com>
2019-04-23 00:52:29 +1000

    unifier continue

--- src/main/java/org/gu/dcore/reasoning/Unifier.java
+++ src/main/java/org/gu/dcore/reasoning/Unifier.java
@@ -1,6 +1,10 @@
 package org.gu.dcore.reasoning;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 
@@ -12,20 +16,19 @@ import org.gu.dcore.model.Variable;
 
 public class Unifier {
 	private Partition partition;
-	private AtomSet pB;
-	private AtomSet H;
+	private Set<Atom> pB;
+	private Set<Atom> H;
 	
 	private AtomSet B;
-	private ExRule rule;
+	private Map<Term, List<Atom>> sticky;
 	
-	private Boolean isPieceUnifier = null;
-	
-	public Unifier(AtomSet _pB, AtomSet _H, Partition _partition, AtomSet _B, ExRule _rule) {
+	public Unifier(Set<Atom> _pB, Set<Atom> _H, Partition _partition, AtomSet _B) {
 		this.pB = _pB;
 		this.H = _H;
 		this.partition = _partition;
 		this.B = _B;
-		this.rule = _rule;
+	
+		sticky = new HashMap<>();
 	}
 	
 	public Atom getImageOf(Atom a) {
@@ -36,28 +39,63 @@ public class Unifier {
 		return null;
 	}
 	
-	public boolean isPieceUnifier() {
-		if(this.isPieceUnifier == null) this.isPieceUnifier = checkPieceUnifier();
-		return this.isPieceUnifier;
+	public Unifier extend(Unifier u) {
+		Set<Atom> newpB = new HashSet<>();
+		Set<Atom> newH = new HashSet<>();
+		
+		this.partition.join(u.getPartition());
 	}
 	
-	private boolean checkPieceUnifier() {
-		Set<Term> separating = new HashSet<>();
-		AtomSet minus = AtomSetUtils.minus(this.B, this.pB);
+	public boolean isPieceUnifier() {
+		if(sticky == null) this.computeSticky();
+		return this.sticky.isEmpty();
+	}
+	
+	public Set<Atom> getStickyAtoms() {
+		if(this.sticky == null) this.computeSticky();
 		
-		Set<Variable> var_minus = minus.getVariables();
+		Set<Atom> re = new HashSet<>();
 		
-		for(Variable v : pB.getVariables()) {
-			if(var_minus.contains(v)) separating.add(v);
+		for(List<Atom> atoms : this.sticky.values()) 
+			re.addAll(atoms);
+		
+		return re;
+	}
+	
+	public Partition getPartition() {
+		return this.partition;
+	}
+	
+	private void computeSticky() {
+		this.sticky = new HashMap<>();
+		List<Atom> minus = new LinkedList<>();
+		
+		for(Atom a : this.B) {
+			if(!this.pB.contains(a)) minus.add(a);
+		}
+		
+		Set<Variable> pBvar = new HashSet<>();
+		for(Atom a : pB) pBvar.addAll(a.getVariable());
+		
+		for(Atom a : minus) {
+			for(Variable v : pBvar) {
+				if(a.getVariable().contains(v)) {
+					List<Atom> sticky_atoms = this.sticky.get(v);
+					if(sticky_atoms == null) {
+						sticky_atoms = new LinkedList<>();
+						sticky_atoms.add(a);
+						this.sticky.put(v, sticky_atoms);
+					}
+					else sticky_atoms.add(a);
+				}
+			}
 		}
 		
 		for(Entry<Set<Term>, boolean[]> category : this.partition.categories.entrySet()) {
 			if(category.getValue()[1]) {
-				for(Term t : separating) 
-					if(category.getKey().contains(t)) return false;
+				for(Term t : this.sticky.keySet()) 
+					if(!category.getKey().contains(t)) this.sticky.remove(t);
 			}
 		}
-		
-		return true;
 	}
 }
--- src/main/java/org/gu/dcore/reasoning/Unify.java
+++ src/main/java/org/gu/dcore/reasoning/Unify.java
@@ -1,8 +1,14 @@
 package org.gu.dcore.reasoning;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Queue;
+import java.util.Set;
 
 import org.gu.dcore.interf.Term;
 import org.gu.dcore.model.Atom;
@@ -16,8 +22,9 @@ public class Unify {
 	}
 	
 	private static List<Unifier> getMostGeneralPreUnifier(ExRule rule, AtomSet atomset) {
-		List<Unifier> preUnifiers = new LinkedList<>();
-		List<Unifier> singlePieceUnifier = new LinkedList<>();
+	//	List<Unifier> preUnifiers = new LinkedList<>();
+		List<Unifier> singlePieceUnifiers = new LinkedList<>();
+		Map<Atom, List<Unifier>> preUnifiers = new HashMap<>();
 		
 		for(Atom a : atomset) {
 			for(Atom b : rule.getHead()) {
@@ -35,33 +42,54 @@ public class Unify {
 					}
 					
 					if(valid) {
-						Unifier u = new Unifier(new AtomSet(b), new AtomSet(a), partition, atomset, rule);
-						if(u.isPieceUnifier()) singlePieceUnifier.add(u);
-						else preUnifiers.add(u);
+						Set<Atom> B = new HashSet<>(); B.add(a);
+						Set<Atom> H = new HashSet<>(); H.add(b);
+						
+						Unifier u = new Unifier(B, H, partition, atomset);
+						if(u.isPieceUnifier()) singlePieceUnifiers.add(u);
+						else {
+							List<Unifier> unifiers = preUnifiers.get(a);
+							if(unifiers == null) {
+								unifiers = new LinkedList<>();
+								unifiers.add(u);
+								preUnifiers.put(a, unifiers);
+							}
+							else unifiers.add(u);
+						}
 					}
 				}
 			}
 		}
 		
-		Queue<Unifier> toExtend = new LinkedList<>();
-		toExtend.addAll(preUnifiers);
-		
-		while(!toExtend.isEmpty()) {
-			Unifier unifier = toExtend.poll();
-			
-			List<Unifier> extended = extend(unifier, atomset, preUnifiers);
-			
-			for(Unifier u : extended) {
-				if(u.isPieceUnifier()) singlePieceUnifier.add(u);
-				else toExtend.add(u);
+		for(Entry<Atom, List<Unifier>> entry : preUnifiers.entrySet()) {
+			Iterator<Unifier> it = entry.getValue().iterator();
+			while(it.hasNext()) {
+				singlePieceUnifiers.addAll(extend(it.next(), preUnifiers));
+				it.remove();
 			}
 		}
 		
-		return singlePieceUnifier;	
+		return singlePieceUnifiers;	
 	}
 	
-	private static List<Unifier> extend(Unifier unifier, AtomSet atomset, List<Unifier> preUnifiers) {
+	private static List<Unifier> extend(Unifier unifier, Map<Atom, List<Unifier>> preUnifiers) {
+		Set<Atom> stickyAtoms = unifier.getStickyAtoms();
 		
+		Queue<Partition> prePartition = new LinkedList<>();
+		prePartition.add(unifier.getPartition());
+		
+		for(Atom a : stickyAtoms) {
+			while(!prePartition.isEmpty()) {
+				Partition p = prePartition.poll();
+				List<Unifier> us = preUnifiers.get(a);
+				
+				if(us == null || us.isEmpty()) continue;
+				
+				for(Unifier u : us) {
+					
+				}
+			}
+		}
 		
 		return null;
 	}
